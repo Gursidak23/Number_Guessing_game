1ad4487da3c346bd365a35f3fb51a6c8e5f14b7f
Gursidak23 <80286868+Gursidak23@users.noreply.github.com>
2023-06-12 16:48:14 +0530

    Add files via upload

---
+++ Number Guessing Game/guess.sql
@@ -0,0 +1,6 @@
+create database GuessGame;
+use GuessGame;
+create table Login(email varchar(30), password varchar(20));
+insert into Login values('demo@gmail.com','pass123');
+select * from Login;
+
